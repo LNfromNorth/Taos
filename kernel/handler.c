a77fc3e4aa7310800f4585b61e67eec7d3622613
LNfromNorth <lnfromnorth@qq.com>
2025-04-28 10:30:43 +0800

    ✨ update the timer_handler function

--- kernel/handler.c
+++ kernel/handler.c
@@ -4,6 +4,8 @@
 
 void ktrapvec();
 
+void timer_intr();
+
 void trap_init() {
     csr_write(stvec, (uint64_t)ktrapvec); 
     printk("[INIT] finish trap init\n");
@@ -11,8 +13,8 @@ void trap_init() {
 
 void trap_on() {
     csr_write(sie, (csr_read(sie) | SIE_STIE));
-    clock_set_next_event();
     csr_write(sstatus, (csr_read(sstatus) | SSTATUS_SIE));
+    timer_intr();
     printk("[INIT] finish trap on\n");
 }
 
@@ -24,11 +26,12 @@ void ktrap_handler() {
     uint64_t scause = csr_read(scause);
     uint64_t sepc = csr_read(sepc);
     if(scause == 0x8000000000000005) {  // timer interrupt
+        timer_intr();
         printk("[DEBUG] Into a ktrap handler\n");
-        clock_set_next_event();
     }
 }
 
-void time_intr() {
-
+void timer_intr() {
+    // now set default function to set next event
+    clock_set_next_event();
 }
